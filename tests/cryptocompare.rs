3fd1e4b2dca7c3a101bff120f94994d5b4da6a88
hekrause <hekrause@htwg-konstanz.de>
2018-01-25 16:08:46 +0100

    :pencil: Make tests to tests.

--- tests/cryptocompare.rs
+++ tests/cryptocompare.rs
@@ -3,87 +3,72 @@ extern crate cryptocompare;
 extern crate serde_json;
 
 use cryptocompare::Options;
-use std::time::Duration;
-use std::thread;
+
 
 #[test]
-#[ignore]
 fn test_get_coin_list() {
-    let _result = cryptocompare::get_coin_list().unwrap();
+
+    let result = cryptocompare::get_coin_list();
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
-#[ignore]
 fn test_get_price() {
     let option: Options = Options {exchanges: "Bitfinex", try_conversion: false};
-
-    loop {
-        let result = cryptocompare::get_price("BTC", "USD", &option).unwrap();
-        println!("{:?}", result.as_object().unwrap()["USD"].as_f64().unwrap());
-        thread::sleep(Duration::from_secs(1))
-    }
+    let result = cryptocompare::get_price("BTC", "USD", &option);
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
-#[ignore]
 fn test_get_price_multi() {
     let option: Options = Options {exchanges: "Bitfinex", try_conversion: false};
-    let _result = cryptocompare::get_price_multi("IOT", "USD,BTC,ETH", &option).unwrap();
+    let result = cryptocompare::get_price_multi("IOT", "USD,BTC,ETH", &option);
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
-#[ignore]
 fn test_get_price_multi_full() {
     let option: Options = Options {exchanges: "Bitfinex", try_conversion: false};
-    let _result = cryptocompare::get_price_multi_full("IOT", "USD,BTC,ETH", &option);
+    let result = cryptocompare::get_price_multi_full("IOT", "USD,BTC,ETH", &option);
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
-#[ignore]
 fn test_get_price_historical() {
     let option: Options = Options { exchanges: "Bitfinex", try_conversion: false };
-    let _result = cryptocompare::get_price_historical("IOT", "USD,BTC,ETH", &option);
+    let result = cryptocompare::get_price_historical("IOT", "USD,BTC,ETH", &option);
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
-#[ignore]
 fn test_get_history_day() {
     let option: Options = Options { exchanges: "Bitfinex", try_conversion: false };
-    let limit: u64 = 10;
-    let _result = cryptocompare::get_history_day("IOT", "USD", &option, &limit);
+    let limit: u64 = 3;
+    let result = cryptocompare::get_history_day("BTC", "USD", &option, &limit);
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
-#[ignore]
 fn test_get_history_hour() {
     let option: Options = Options { exchanges: "Bitfinex", try_conversion: false };
-    let limit: u64 = 10;
-    let _result = cryptocompare::get_history_hour("IOT", "USD", &option, &limit);
+    let limit: u64 = 3;
+    let result = cryptocompare::get_history_hour("BTC", "USD", &option, &limit);
+    assert_eq!(result.is_err(), false)
 }
 
 #[test]
 fn test_get_history_minute() {
     let option: Options = Options { exchanges: "Bitfinex", try_conversion: false };
-    let limit: u64 = 1000;
+    let limit: u64 = 3;
+    let result = cryptocompare::get_history_minute("BTC", "USD", &option, &limit);
+    assert_eq!(result.is_err(), false)
+}
 
-    loop {
-        let result = cryptocompare::get_history_minute("BTC", "USD", &option, &limit).unwrap();
-        println!("{:#?}", result);
-        thread::sleep(Duration::from_secs(60))
-    }
-
-    /*
-    loop {
-        let result = cryptocompare::get_history_minute("BTC", "USD", &option, &limit).unwrap();
-        let data = result["Data"].as_array().unwrap();
-
-        let open = &data[0]["open"].as_f64().unwrap();
-        let close = &data[0]["close"].as_f64().unwrap();
-        let high = &data[0]["high"].as_f64().unwrap();
-        let low = &data[0]["low"].as_f64().unwrap();
-
-        println!("open: {:?}; close: {:?}; high: {:?}; low: {:?}", open, close, high, low);
-        thread::sleep(Duration::from_secs(60))
-    }
-    */
+#[test]
+fn test_parse_json_to_vector() {
+    let option: Options = Options { exchanges: "Bitfinex", try_conversion: false };
+    let limit: u64 = 3;
+    let result = cryptocompare::get_history_minute("BTC", "USD", &option, &limit).unwrap();
+    let vector = cryptocompare::parse_json_to_vector(result).unwrap();
+    assert_eq!(vector.len(), (limit + 1) as usize)
 }
