373034c9e2b89148faad5b8d7486d4402bc5898a
hekrause <hekrause@htwg-konstanz.de>
2018-01-25 16:08:15 +0100

    :pencil: Add parsing helper.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 
+#![feature(try_trait)]
+
 extern crate reqwest;
 extern crate serde_json;
 extern crate time;
@@ -11,6 +13,17 @@ static BASE_URL: &str = "https://min-api.cryptocompare.com/data/";
 pub struct Options<'a> {
     pub exchanges: &'a str,
     pub try_conversion: bool
+}
+
+#[derive(Debug)]
+pub struct Dataset {
+    pub close: f64,
+    pub high: f64,
+    pub low: f64,
+    pub open: f64,
+    pub time: i64,
+    pub volumefrom: f64,
+    pub volumeto: f64
 }
 
 pub fn get_coin_list() -> Result<serde_json::Value, Box<::std::error::Error>> {
@@ -105,4 +118,23 @@ pub fn get_history_minute(fsym: &str, tsym: &str, options: &Options, limit: &u64
     let value: Value = serde_json::from_str(&mut result)?;
 
     Ok(value)
+}
+
+pub fn parse_json_to_vector(object: Value) -> Result<Vec<Dataset>, Box<::std::option::NoneError>> {
+    let mut vector: Vec<Dataset> = Vec::new();
+    let array = object.as_object()?["Data"].as_array()?;
+
+    for entry in array {
+        let elem = entry.as_object()?;
+        vector.push(Dataset {
+            close: elem["close"].as_f64()?,
+            high: elem["high"].as_f64()?,
+            low: elem["low"].as_f64()?,
+            open: elem["open"].as_f64()?,
+            time: elem["time"].as_i64()?,
+            volumefrom: elem["volumefrom"].as_f64()?,
+            volumeto: elem["volumeto"].as_f64()?
+        })
+    }
+    Ok(vector)
 }
